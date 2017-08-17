97bb892740571474106a950105a499b2a2c71862
Ville Tikka <tikka.ville@gmail.com>
2017-08-17 15:29:49 +0300

    Added Elbas support

--- example.py
+++ example.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
 # Import library for fetching Elspot data
-from nordpool import elspot
+from nordpool import elspot, elbas
 from pprint import pprint
 
 # Initialize class for fetching Elspot prices
-prices = elspot.Prices()
-# Fetch hourly prices for Finland and print the resulting dictionary
-pprint(prices.hourly(areas=['FI']))
+prices_spot = elspot.Prices()
+
+# Initialize class for fetching Elsbas prices
+prices_bas = elbas.Prices()
+
+# Fetch hourly Elspot prices for Finland and print the resulting dictionary
+pprint(prices_spot.hourly(areas=['FI']))
+
+# Fetch hourly Elbas prices for Finland and print the resulting dictionary
+pprint(prices_bas.hourly(areas=['FI']))
