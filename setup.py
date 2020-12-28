44fea3785ddbd25c9d5199e75e641f9cb0c833b4
Kimmo Huoman <kipenroskaposti@gmail.com>
2020-12-28 16:51:23 +0200

    Bump version number.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name='nordpool',
-    version='0.3.0',
+    version='0.3.1',
     description='Python library for fetching Nord Pool spot prices.',
     author='Kimmo Huoman',
     author_email='kipenroskaposti@gmail.com',
