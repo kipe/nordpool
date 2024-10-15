3e50dc0786faf66031f614d39fa57059647b54ec
Kimmo Huoman <kipenroskaposti@gmail.com>
2024-10-15 21:28:57 +0300

    Fix price fetching, add tests, switch to poetry for package management and publishing

--- example.py
+++ example.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python
 
 # Import library for fetching Elspot data
-from nordpool import elspot, elbas
+from nordpool import elspot
 from pprint import pprint
 
 # Initialize class for fetching Elspot prices
 prices_spot = elspot.Prices()
 
-# Initialize class for fetching Elsbas prices
-prices_bas = elbas.Prices()
-
 # Fetch hourly Elspot prices for Finland and print the resulting dictionary
-pprint(prices_spot.hourly(areas=['FI']))
-
-# Fetch hourly Elbas prices for Finland and print the resulting dictionary
-pprint(prices_bas.hourly(areas=['FI']))
+pprint(prices_spot.hourly(areas=["FI"]))
