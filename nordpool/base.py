584afa24d807768bbdef326ef1c6a2a588121484
Kimmo Huoman <kipenroskaposti@gmail.com>
2024-10-15 21:28:57 +0300

    Fix price fetching, add tests, switch to poetry for package management and publishing

--- nordpool/base.py
+++ nordpool/base.py
@@ -1,5 +1,3 @@
-# -*- encoding: utf-8 -*-
-from __future__ import unicode_literals
 from dateutil.parser import parse as parse_dt
 from pytz import timezone, utc
 
@@ -9,21 +7,22 @@ class CurrencyMismatch(ValueError):
 
 
 class Base(object):
-    ''' Class for fetching Nord Pool Elspot prices. '''
-    def __init__(self, currency='EUR', timeout=None):
+    """Class for fetching Nord Pool Elspot prices."""
+
+    def __init__(self, currency="EUR", timeout=None):
         self.currency = currency
-        self.timeout = timeout
+        self.timeout = timeout or 2
 
     def _parse_dt(self, time_str):
-        ''' Parse datetimes to UTC from Stockholm time, which Nord Pool uses. '''
+        """Parse datetimes to UTC from Stockholm time, which Nord Pool uses."""
         time = parse_dt(time_str)
         if time.tzinfo is None:
-            return timezone('Europe/Stockholm').localize(time).astimezone(utc)
+            return timezone("Europe/Stockholm").localize(time).astimezone(utc)
         return time
 
     def _conv_to_float(self, s):
-        ''' Convert numbers to float. Return infinity, if conversion fails. '''
+        """Convert numbers to float. Return infinity, if conversion fails."""
         try:
-            return float(s.replace(',', '.').replace(" ", ""))
+            return float(s.replace(",", ".").replace(" ", ""))
         except ValueError:
-            return float('inf')
+            return float("inf")
--- nordpool/elspot.py
+++ nordpool/elspot.py
@@ -1,23 +1,53 @@
-# -*- encoding: utf-8 -*-
-from __future__ import unicode_literals
-import requests
 from datetime import date, datetime, timedelta
+
+from pytz import utc
+import requests
 from dateutil.parser import parse as parse_dt
+
 from .base import Base, CurrencyMismatch
 
 
 class Prices(Base):
-    ''' Class for fetching Nord Pool Elspot prices. '''
+    """Class for fetching Nord Pool Elspot prices."""
+
     HOURLY = 10
     DAILY = 11
     WEEKLY = 12
     MONTHLY = 13
     YEARLY = 14
 
-    API_URL = 'https://www.nordpoolgroup.com/api/marketdata/page/%i'
+    API_URL = "https://dataportal-api.nordpoolgroup.com/api"
 
-    def _parse_json(self, data, areas=[]):
-        '''
+    AREAS = [
+        # Nordics
+        "DK1",
+        "DK2",
+        "FI",
+        "NO1",
+        "NO2",
+        "NO3",
+        "NO4",
+        "SE1",
+        "SE2",
+        "SE3",
+        "SE4",
+        # Baltic
+        "EE",
+        "LT",
+        "LV",
+        # CWE
+        "AT",
+        "BE",
+        "FR",
+        "GER",
+        "NL",
+        "PL",
+        # Nordic system price
+        "SYS",
+    ]
+
+    def _parse_json(self, data, data_type, areas):
+        """
         Parse json response from fetcher.
         Returns dictionary with
             - start time
@@ -27,88 +57,111 @@ class Prices(Base):
             - dictionary of areas, based on selection
                 - list of values (dictionary with start and endtime and value)
                 - possible other values, such as min, max, average for hourly
-        '''
 
-        # If areas isn't a list, make it one
-        if not isinstance(areas, list):
-            areas = list(areas)
+        """
 
-        # Update currency from data
-        currency = data['currency']
+        start_time = datetime(
+            2100,
+            1,
+            1,
+            tzinfo=utc if data_type == self.HOURLY else None,
+        )
+        end_time = datetime(
+            1970,
+            1,
+            1,
+            tzinfo=utc if data_type == self.HOURLY else None,
+        )
+        updated = parse_dt(data.get("updatedAt", "1970-01-01"))
+        currency = data.get("currency", self.currency)
+
+        area_prices = {}
+        data_source = ("multiAreaEntries", "entryPerArea")  # Defaults to HOURLY
+        if data_type == self.DAILY:
+            data_source = ("multiAreaDailyAggregates", "averagePerArea")
+        if data_type == self.WEEKLY:
+            data_source = ("multiAreaWeeklyAggregates", "averagePerArea")
+        if data_type == self.MONTHLY:
+            data_source = ("multiAreaMonthlyAggregates", "averagePerArea")
+        if data_type == self.YEARLY:
+            data_source = ("prices", "averagePerArea")
+
+        for entry in data.get(data_source[0], []):
+            start = parse_dt(entry["deliveryStart"])
+            end = parse_dt(entry["deliveryEnd"])
+            for area, price in entry.get(data_source[1], {}).items():
+                if area not in areas:
+                    continue
+                if area not in area_prices:
+                    area_prices[area] = []
+                area_prices[area].append(
+                    {
+                        "start": start,
+                        "end": end,
+                        "value": price,
+                    }
+                )
+                start_time = min(start_time, start)
+                end_time = max(end_time, end)
 
         # Ensure that the provided currency match the requested one
         if currency != self.currency:
             raise CurrencyMismatch
 
-        # All relevant data is in data['data']
-        data = data['data']
-        start_time = self._parse_dt(data['DataStartdate'])
-        end_time = self._parse_dt(data['DataEnddate'])
-        updated = self._parse_dt(data['DateUpdated'])
-
-        area_data = {}
-        # Loop through response rows
-        for r in data['Rows']:
-            row_start_time = self._parse_dt(r['StartTime'])
-            row_end_time = self._parse_dt(r['EndTime'])
-
-            # Loop through columns
-            for c in r['Columns']:
-                name = c['Name']
-                # If areas is defined and name isn't in areas, skip column
-                if areas and name not in areas:
-                    continue
-
-                # If name isn't in area_data, initialize dictionary
-                if name not in area_data:
-                    area_data[name] = {
-                        'values': [],
-                    }
-
-                # Time based and average, max, min etc rows are separated
-                # with 'IsExtraRow' -marking
-                if r['IsExtraRow']:
-                    # Update extra data to dictionary
-                    area_data[name][r['Name']] = self._conv_to_float(c['Value'])
-                else:
-                    # Append dictionary to value list
-                    area_data[name]['values'].append({
-                        'start': row_start_time,
-                        'end': row_end_time,
-                        'value': self._conv_to_float(c['Value']),
-                    })
-
         return {
-            'start': start_time,
-            'end': end_time,
-            'updated': updated,
-            'currency': currency,
-            'areas': area_data
+            "start": start_time,
+            "end": end_time,
+            "updated": updated,
+            "currency": currency,
+            "areas": area_prices,
         }
 
-    def _fetch_json(self, data_type, end_date=None):
-        ''' Fetch JSON from API '''
+    def _get_url_params_areas(self, data_type, end_date=None, areas=None):
         # If end_date isn't set, default to tomorrow
         if end_date is None:
             end_date = date.today() + timedelta(days=1)
         # If end_date isn't a date or datetime object, try to parse a string
         if not isinstance(end_date, date) and not isinstance(end_date, datetime):
             end_date = parse_dt(end_date)
+        if areas is None:
+            areas = self.AREAS
 
-        # Create request to API
-        r = requests.get(self.API_URL % data_type, params={
-            'currency': self.currency,
-            'endDate': end_date.strftime('%d-%m-%Y'),
-        }, timeout=self.timeout)
-        # Return JSON response
-        return r.json()
+        endpoint = "DayAheadPrices"  # default to hourly
+        if data_type in [self.DAILY, self.WEEKLY, self.MONTHLY]:
+            endpoint = "AggregatePrices"
+        if data_type == self.YEARLY:
+            endpoint = "AggregatePrices/GetAnnuals"
 
-    def fetch(self, data_type, end_date=None, areas=[]):
-        '''
+        api_url = f"{self.API_URL}/{endpoint}"
+        params = {
+            "currency": "EUR",
+            "market": "DayAhead",
+            "deliveryArea": ",".join(areas),
+        }
+
+        if data_type == self.HOURLY:
+            params["date"] = end_date.strftime("%Y-%m-%d")
+        if data_type in [self.DAILY, self.WEEKLY, self.MONTHLY]:
+            params["year"] = end_date.strftime("%Y")
+        return api_url, params, areas
+
+    def _fetch_json(self, data_type, end_date=None, areas=None):
+        """Fetch JSON from API"""
+        api_url, params, areas = self._get_url_params_areas(data_type, end_date, areas)
+        response = requests.get(
+            api_url,
+            params=params,
+            timeout=self.timeout,
+        )
+        response.raise_for_status()
+        return self._parse_json(response.json(), data_type, areas)
+
+    def fetch(self, data_type, end_date=None, areas=None):
+        """
         Fetch data from API.
         Inputs:
             - data_type
-                API page id, one of Prices.HOURLY, Prices.DAILY etc
+                one of Prices.HOURLY, Prices.DAILY etc
             - end_date
                 datetime to end the data fetching
                 defaults to tomorrow
@@ -124,27 +177,28 @@ class Prices(Base):
             - dictionary of areas, based on selection
                 - list of values (dictionary with start and endtime and value)
                 - possible other values, such as min, max, average for hourly
-        '''
-        return self._parse_json(self._fetch_json(data_type, end_date), areas)
+        """
 
-    def hourly(self, end_date=None, areas=[]):
-        ''' Helper to fetch hourly data, see Prices.fetch() '''
+        return self._fetch_json(data_type, end_date, areas)
+
+    def hourly(self, end_date=None, areas=None):
+        """Helper to fetch hourly data, see Prices.fetch()"""
         return self.fetch(self.HOURLY, end_date, areas)
 
-    def daily(self, end_date=None, areas=[]):
-        ''' Helper to fetch daily data, see Prices.fetch() '''
+    def daily(self, end_date=None, areas=None):
+        """Helper to fetch daily data, see Prices.fetch()"""
         return self.fetch(self.DAILY, end_date, areas)
 
-    def weekly(self, end_date=None, areas=[]):
-        ''' Helper to fetch weekly data, see Prices.fetch() '''
+    def weekly(self, end_date=None, areas=None):
+        """Helper to fetch weekly data, see Prices.fetch()"""
         return self.fetch(self.WEEKLY, end_date, areas)
 
-    def monthly(self, end_date=None, areas=[]):
-        ''' Helper to fetch monthly data, see Prices.fetch() '''
+    def monthly(self, end_date=None, areas=None):
+        """Helper to fetch monthly data, see Prices.fetch()"""
         return self.fetch(self.MONTHLY, end_date, areas)
 
-    def yearly(self, end_date=None, areas=[]):
-        ''' Helper to fetch yearly data, see Prices.fetch() '''
+    def yearly(self, end_date=None, areas=None):
+        """Helper to fetch yearly data, see Prices.fetch()"""
         return self.fetch(self.YEARLY, end_date, areas)
 
 
@@ -154,9 +208,10 @@ class AioPrices(Prices):
         super().__init__(currency)
         self.client = client
 
-    async def _io(self, url, **kwargs):
+    async def _io(self, url, params):
         import inspect
-        resp = await self.client.get(url, params=kwargs)
+
+        resp = await self.client.get(url, params=params)
         # aiohttp
         if inspect.isawaitable(resp.json()):
             return await resp.json()
@@ -164,22 +219,15 @@ class AioPrices(Prices):
             # Httpx and asks
             return resp.json()
 
-    async def _fetch_json(self, data_type, end_date=None):
-        """ Fetch JSON from API """
-        # If end_date isn't set, default to tomorrow
-        if end_date is None:
-            end_date = date.today() + timedelta(days=1)
-        # If end_date isn't a date or datetime object, try to parse a string
-        if not isinstance(end_date, date) and not isinstance(end_date, datetime):
-            end_date = parse_dt(end_date)
-
+    async def _fetch_json(self, data_type, end_date=None, areas=None):
+        """Fetch JSON from API"""
+        api_url, params, areas = self._get_url_params_areas(data_type, end_date, areas)
         return await self._io(
-            self.API_URL % data_type,
-            currency=self.currency,
-            endDate=end_date.strftime("%d-%m-%Y"),
+            api_url,
+            params,
         )
 
-    async def fetch(self, data_type, end_date=None, areas=[]):
+    async def fetch(self, data_type, end_date=None, areas=None):
         """
         Fetch data from API.
         Inputs:
@@ -200,25 +248,25 @@ class AioPrices(Prices):
                 - list of values (dictionary with start and endtime and value)
                 - possible other values, such as min, max, average for hourly
         """
-        data = await self._fetch_json(data_type, end_date)
-        return self._parse_json(data, areas)
+        data = await self._fetch_json(data_type, end_date, areas=areas)
+        return self._parse_json(data, data_type, areas)
 
-    async def hourly(self, end_date=None, areas=[]):
-        """ Helper to fetch hourly data, see Prices.fetch() """
+    async def hourly(self, end_date=None, areas=None):
+        """Helper to fetch hourly data, see Prices.fetch()"""
         return await self.fetch(self.HOURLY, end_date, areas)
 
-    async def daily(self, end_date=None, areas=[]):
-        """ Helper to fetch daily data, see Prices.fetch() """
+    async def daily(self, end_date=None, areas=None):
+        """Helper to fetch daily data, see Prices.fetch()"""
         return await self.fetch(self.DAILY, end_date, areas)
 
-    async def weekly(self, end_date=None, areas=[]):
-        """ Helper to fetch weekly data, see Prices.fetch() """
+    async def weekly(self, end_date=None, areas=None):
+        """Helper to fetch weekly data, see Prices.fetch()"""
         return await self.fetch(self.WEEKLY, end_date, areas)
 
-    async def monthly(self, end_date=None, areas=[]):
-        """ Helper to fetch monthly data, see Prices.fetch() """
+    async def monthly(self, end_date=None, areas=None):
+        """Helper to fetch monthly data, see Prices.fetch()"""
         return await self.fetch(self.MONTHLY, end_date, areas)
 
-    async def yearly(self, end_date=None, areas=[]):
-        """ Helper to fetch yearly data, see Prices.fetch() """
+    async def yearly(self, end_date=None, areas=None):
+        """Helper to fetch yearly data, see Prices.fetch()"""
         return await self.fetch(self.YEARLY, end_date, areas)
