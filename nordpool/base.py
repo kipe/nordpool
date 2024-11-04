c4739ab706318a2d4b60db328219e2f827936385
Kimmo Huoman <kipenroskaposti@gmail.com>
2024-11-04 10:06:37 +0200

    Add no covers for un-tested parts

--- nordpool/base.py
+++ nordpool/base.py
@@ -1,6 +1,4 @@
 # pylint: disable=missing-module-docstring
-from dateutil.parser import parse as parse_dt
-from pytz import timezone, utc
 
 
 class CurrencyMismatch(ValueError):  # pylint: disable=missing-class-docstring
@@ -13,17 +11,3 @@ class Base:  # pylint: disable=too-few-public-methods
     def __init__(self, currency="EUR", timeout=None):
         self.currency = currency
         self.timeout = timeout or 2
-
-    def _parse_dt(self, time_str):
-        """Parse datetimes to UTC from Stockholm time, which Nord Pool uses."""
-        time = parse_dt(time_str)
-        if time.tzinfo is None:
-            return timezone("Europe/Stockholm").localize(time).astimezone(utc)
-        return time
-
-    def _conv_to_float(self, s):
-        """Convert numbers to float. Return infinity, if conversion fails."""
-        try:
-            return float(s.replace(",", ".").replace(" ", ""))
-        except ValueError:
-            return float("inf")
