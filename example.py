52d9c2e8e0f94866b88d4d9bc06cc84cf637e037
flopp999 <21694965+flopp999@users.noreply.github.com>
2025-05-11 12:54:49 +0200

    Update example.py

--- example.py
+++ example.py
@@ -5,9 +5,9 @@ from nordpool import elspot
 from pprint import pprint
 
 # Initialize class for fetching Elspot prices
-prices_spot = elspot.Prices()
+prices_spot = elspot.Prices() # fetch data in Euro
 
-# Fetch hourly Elspot prices for Finland and print the resulting dictionary.
+# Fetch Elspot prices and print the resulting dictionary
 # If the prices are reported as None, it means that the prices fetched aren't yet available.
 # The library by default tries to fetch prices for tomorrow and they're released ~13:00 Swedish time.
-pprint(prices_spot.hourly(areas=["FI"]))
+pprint(prices_spot.fetch(end_date=datetime.now().date(),areas=['FI'])) # todays hourly prices for Finland
